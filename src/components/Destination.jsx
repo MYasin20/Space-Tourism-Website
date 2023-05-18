2733daa085c3bcfbe1a373044e417b048da935a4
MYasin <yasin.ego10@gmail.com>
2023-05-18 20:14:48 +0800

    added functionality in Destination Page

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react"
+// import DestinationsSelection from "./DestinationsSelection";
 
 function Destination() {
+  const [selectedPlanet, setSelectedPlanet] = useState({
+    name : '',
+    image: '',
+    details: '',
+    distance: '',
+    travel: ''
+  });
+  const destinationsDB = [
+    { name : 'moon',
+      image: '/src/images/destination/image-moon.png',
+      details: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+      distance: '384,400 Km',
+      travel: '3 Days' 
+    },
+    { name : 'mars',
+      image: '/src/images/destination/image-mars.png',
+      details: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+      distance: '225 MIL. km',
+      travel: '9 months' 
+    },
+    { name : 'europa',
+      image: '/src/images/destination/image-europa.png',
+      details: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+      distance: '628 MIL. km',
+      travel: '3 years' 
+    },
+    { name : 'titan',
+      image: '/src/images/destination/image-titan.png',
+      details: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+      distance: '1.6 BIL. km',
+      travel: '7 years' 
+    },
+  ];
+  useEffect(() => {
+    setSelectedPlanet(destinationsDB[0]);
+  }, [])
+  /* create function that handle the selected destination
+    check the 'name' of object and compare to the clicked
+      filter the output and display the new result
+      ---------------------------------------------
+      in database display first index only (marked as higlight in display)
+      
+  */
+  function handleDestination(e) {
+    const selectedDestination = e.target.textContent.toLowerCase();
+    const newDestination = destinationsDB.find((destination) => destination.name === selectedDestination);
+    setSelectedPlanet(newDestination);
+  }
   return (
     <div className="destination-bg">
 
@@ -10,33 +60,33 @@ function Destination() {
             <span className="destination-number">01</span>
             Pick Your Destination
           </h5>
-          <img src="/src/images/destination/image-moon.png" alt="moon" />
+          <img src={selectedPlanet.image} alt={selectedPlanet.name} />
         </div>
 
         <div className="current-destination">
           <ul className="destinations-selection">
-            <li><a>Moon</a></li>
-            <li><a>Mars</a></li>
-            <li><a>Europa</a></li>
-            <li><a>Titan</a></li>
+            <li onClick={handleDestination}><a>Moon</a></li>
+            <li onClick={handleDestination}><a>Mars</a></li>
+            <li onClick={handleDestination}><a>Europa</a></li>
+            <li onClick={handleDestination}><a>Titan</a></li>
           </ul>
-
+          
           <div className="destination-information">
-            <h2 className="heading-2">Moon</h2>
-            <p className="body-text">See our planet as you&apos;ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+            <h2 className="heading-2">{selectedPlanet.name}</h2>
+            <p className="body-text">{selectedPlanet.details}</p>
             
             <div className="avg-travel">
               <div className="avg-distance">
-                <div className="sub-head-2">Avg.  Distance</div>
-                <div className="des-head-2">384,400 km</div> {/* no css style yet */}
+                <div className="sub-head-2">Avg. Distance</div>
+                <div className="des-head-2">{selectedPlanet.distance}</div> 
               </div>
               <div className="est-travel">
                 <div className="sub-head-2">Est. Travel Time</div>
-                <div className="des-head-2">3 Days</div> {/* no css style yet */}
+                <div className="des-head-2">{selectedPlanet.travel}</div> 
               </div>
             </div>
           </div>
-
+          {/* <DestinationsSelection /> */}
         </div>
       </div>
 
