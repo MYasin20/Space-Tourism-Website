f49525f30c536fa85c15e99db87e9d191191c2e2
MYasin <yasin.ego10@gmail.com>
2023-05-22 03:06:33 +0800

    added responsive tablet and mobile mode in Crew Page

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -2,34 +2,32 @@
 function Crew() {
   return (
     <div className="crew-bg">
+      <div className="crew-container">
+        <h5 className="sub-heading-5">
+          <span className="section-title">02</span>
+            Meet Your Crew
+        </h5>
 
-      <div className="crew">
-
-        <div className="crew-left-side">
-          <h5 className="sub-heading-5">
-            <span className="section-title">02</span>
-             Meet Your Crew
-          </h5>
-
+        <div className="crew">
           <div className="crew-information">
             <h4 className="heading-4">Commander</h4>
             <h3 className="heading-3">Douglas Hurley</h3>
             <p className="body-text">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
-          </div>
-
-          <div className="page">
-            <span className="btn-dot dot-active"></span>
-            <span className="btn-dot"></span>
-            <span className="btn-dot"></span>
-            <span className="btn-dot"></span>
-          </div>
           
+            <div className="page">
+              <span className="btn-dot dot-active"></span>
+              <span className="btn-dot"></span>
+              <span className="btn-dot"></span>
+              <span className="btn-dot"></span>
+            </div>
+          </div>
+
+          <div className="crew-img-container">
+            <img className="crew-img" src="/src/images/crew/image-douglas-hurley.png"/>
+          </div>
         </div>
-
-        <img className="crew-image" src="/src/images/crew/image-douglas-hurley.png"/>
-
+        
       </div>
-
     </div>
   )
 }
