7dfb0cfafa5248f391e49964a5fb24d699f01487
MYasin <yasin.ego10@gmail.com>
2023-05-22 09:59:20 +0800

    fix stlying from crew page

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -3,13 +3,12 @@ function Crew() {
   return (
     <div className="crew-bg">
       <div className="crew-container">
-        <h5 className="sub-heading-5">
-          <span className="section-title">02</span>
-            Meet Your Crew
-        </h5>
-
         <div className="crew">
           <div className="crew-information">
+            <h5 className="sub-heading-5">
+              <span className="section-title">02</span>
+                Meet Your Crew
+            </h5>
             <h4 className="heading-4">Commander</h4>
             <h3 className="heading-3">Douglas Hurley</h3>
             <p className="body-text">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
