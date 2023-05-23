4902e1bf72b927dfa5d8c379cc1bcc696b0f1140
MYasin <yasin.ego10@gmail.com>
2023-05-23 20:53:45 +0800

    added functionality of hover and clicking active states

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { destinationsDB } from "../utils/database";
 
 function Destination() {
   const [selectedPlanet, setSelectedPlanet] = useState({
@@ -8,40 +9,20 @@ function Destination() {
     distance: '',
     travel: ''
   });
-  const destinationsDB = [
-    { name : 'moon',
-      image: '/src/images/destination/image-moon.png',
-      details: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
-      distance: '384,400 Km',
-      travel: '3 Days' 
-    },
-    { name : 'mars',
-      image: '/src/images/destination/image-mars.png',
-      details: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
-      distance: '225 MIL. km',
-      travel: '9 months' 
-    },
-    { name : 'europa',
-      image: '/src/images/destination/image-europa.png',
-      details: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
-      distance: '628 MIL. km',
-      travel: '3 years' 
-    },
-    { name : 'titan',
-      image: '/src/images/destination/image-titan.png',
-      details: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
-      distance: '1.6 BIL. km',
-      travel: '7 years' 
-    },
-  ];
+  const [isSelected, setIsSelected] = useState(null);
+  
   useEffect(() => {
     setSelectedPlanet(destinationsDB[0]);
   }, [])
   
   function handleDestination(e) {
+    console.log(e.target.textContent);
     const selectedDestination = e.target.textContent.toLowerCase();
     const newDestination = destinationsDB.find((destination) => destination.name === selectedDestination);
     setSelectedPlanet(newDestination);
+  }
+  function isUnderlineSelected(item) {
+    setIsSelected(item);
   }
   return (
     <div className="destination-bg">
@@ -58,10 +39,34 @@ function Destination() {
 
         <div className="current-destination">
           <ul className="destinations-selection">
-            <li onClick={handleDestination}><a>Moon</a></li>
-            <li onClick={handleDestination}><a>Mars</a></li>
-            <li onClick={handleDestination}><a>Europa</a></li>
-            <li onClick={handleDestination}><a>Titan</a></li>
+            <li className={isSelected === 'Moon' && 'selected'} 
+              onClick={(e) => {
+                isUnderlineSelected('Moon')
+                handleDestination(e)
+              }}>
+              <a>Moon</a>
+            </li>
+            <li className={isSelected === 'Mars' && 'selected'} 
+              onClick={(e) => {
+                isUnderlineSelected('Mars')
+                handleDestination(e)
+              }}>
+              <a>Mars</a>
+            </li>
+            <li className={isSelected === 'Europa' && 'selected'} 
+              onClick={(e) => {
+                isUnderlineSelected('Europa')
+                handleDestination(e)
+              }}>
+              <a>Europa</a>
+            </li>
+            <li className={isSelected === 'Titan' && 'selected'} 
+              onClick={(e) => {
+                isUnderlineSelected('Titan')
+                handleDestination(e)
+              }}>
+              <a>Titan</a>
+            </li>
           </ul>
           
           <div className="destination-information">
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 
 function Navigation() {
+  const [isSelected, setIsSelected] = useState('HOME');
+  function isUnderlineSelected(item) {
+    setIsSelected(item);
+  }
   return (
     <nav className="nav">
       <div className="logo">
@@ -10,25 +15,33 @@ function Navigation() {
       <div className="line"></div>
       <button className="burger-menu"></button>
       <ul className="nav-lists">
-        <li className="list-item is-underline-selected">
+        <li 
+          className={`list-item ${isSelected === 'HOME' && 'selected'}`}
+          onClick={() => isUnderlineSelected('HOME')}>
           <a href="/#">
             <span className="list-item-number">00</span>
             HOME
           </a>
         </li>
-        <li className="list-item">
+        <li 
+          className={`list-item ${isSelected === 'DESTINATION' && 'selected'}`}
+          onClick={() => isUnderlineSelected('DESTINATION')}>
           <a href="/#">
             <span className="list-item-number">01</span>
             DESTINATION
           </a>
         </li>
-        <li className="list-item">
+        <li 
+          className={`list-item ${isSelected === 'CREW' && 'selected'}`}
+          onClick={() => isUnderlineSelected('CREW')}>
           <a href="/#">
             <span className="list-item-number">02</span>
             CREW
           </a>
         </li>
-        <li className="list-item">
+        <li 
+          className={`list-item ${isSelected === 'TECHNOLOGY' && 'selected'}`}
+          onClick={() => isUnderlineSelected('TECHNOLOGY')}>
           <a href="/#">
             <span className="list-item-number">03</span>
             TECHNOLOGY
