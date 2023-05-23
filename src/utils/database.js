29ab974e859659fb0fb46a5f2df1a44b261bf3af
MYasin <yasin.ego10@gmail.com>
2023-05-23 20:52:50 +0800

    added database that store the every pages information

---
+++ src/utils/database.js
@@ -0,0 +1,28 @@
+const destinationsDB = [
+  { name : 'moon',
+    image: '/src/images/destination/image-moon.png',
+    details: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    distance: '384,400 Km',
+    travel: '3 Days' 
+  },
+  { name : 'mars',
+    image: '/src/images/destination/image-mars.png',
+    details: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+    distance: '225 MIL. km',
+    travel: '9 months' 
+  },
+  { name : 'europa',
+    image: '/src/images/destination/image-europa.png',
+    details: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+    distance: '628 MIL. km',
+    travel: '3 years' 
+  },
+  { name : 'titan',
+    image: '/src/images/destination/image-titan.png',
+    details: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+    distance: '1.6 BIL. km',
+    travel: '7 years' 
+  },
+];
+
+export { destinationsDB };
