e2de56079b1887b0c8133740db558df5df570155
MYasin <yasin.ego10@gmail.com>
2023-05-26 03:29:26 +0800

    added when button is clicked it changes to the different crews

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { crewsDB } from "../utils/database";
+
 
 function Crew() {
+  const [selectedCrew, setSelectedCrew] = useState({
+    position: '',
+    crewName: '',
+    details: '',
+    image: ''
+  })
+  const [isSelected, setIsSelected] = useState(1);
+
+  useEffect(() => {
+    setSelectedCrew(crewsDB[0]);
+  },[])
+
+  function handleSelectedCrew(e) {
+    const crewSelected = e.target.getAttribute('data-page-index');
+    console.log(crewSelected);
+    const newCrewSelected = crewsDB.find(crew => crew.id === Number(crewSelected))
+    setSelectedCrew(newCrewSelected);
+  }
+
+  function isUnderlineSelected(num) {
+    setIsSelected(num);
+  }
+
   return (
     <div className="crew-bg">
       <div className="crew-container">
@@ -9,20 +35,48 @@ function Crew() {
         </h5>
 
         <div className="crew-information">
-          <h4 className="heading-4">Commander</h4>
-          <h3 className="heading-3">Douglas Hurley</h3>
-          <p className="body-text">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+          <h4 className="heading-4">{selectedCrew.position}</h4>
+          <h3 className="heading-3">{selectedCrew.crewName}</h3>
+          <p className="body-text">{selectedCrew.details}</p>
         </div>
 
         <div className="page">
-            <span className="btn-dot dot-active"></span>
-            <span className="btn-dot"></span>
-            <span className="btn-dot"></span>
-            <span className="btn-dot"></span>
+            <span 
+              onClick={(e) => {
+                handleSelectedCrew(e);
+                isUnderlineSelected(1)
+              }}
+              data-page-index='1' 
+              className={`btn-dot${isSelected === 1 ? ' dot-active' : ''}`}>
+            </span>
+            <span 
+              onClick={(e) => {
+                handleSelectedCrew(e);
+                isUnderlineSelected(2)
+              }} 
+              data-page-index='2'
+              className={`btn-dot${isSelected === 2 ? ' dot-active' : ''}`}>
+            </span>
+            <span 
+              onClick={(e) => {
+                handleSelectedCrew(e);
+                isUnderlineSelected(3)
+              }} 
+              data-page-index='3' 
+              className={`btn-dot${isSelected === 3 ? ' dot-active' : ''}`}>
+            </span>
+            <span 
+              onClick={(e) => {
+                handleSelectedCrew(e);
+                isUnderlineSelected(4)
+              }} 
+              data-page-index='4'
+              className={`btn-dot${isSelected === 4 ? ' dot-active' : ''}`}>
+            </span>
         </div>
 
         <div className="crew-img-container">
-          <img className="crew-img" src="/src/images/crew/image-douglas-hurley.png"/>
+          <img className="crew-img" src={selectedCrew.image} alt={selectedCrew.crewName}/>
         </div>
         
       </div>
