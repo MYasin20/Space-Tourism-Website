92b50af01e0ac8adda48af38eb968dd7d9130c86
MYasin <yasin.ego10@gmail.com>
2023-05-26 22:34:00 +0800

    added database on technology

--- src/utils/database.js
+++ src/utils/database.js
@@ -54,6 +54,30 @@ const crewsDB =[
     details: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ',
     image: '/src/images/crew/image-anousheh-ansari.png',
   },
+];
+
+const technologyDB = [
+  {
+    id: 1,
+    techName: 'launch vehicle',
+    imgLandscape: '/src/images/technology/image-launch-vehicle-landscape.png',
+    imgPortrait: '/src/images/technology/image-launch-vehicle-portrait.jpg',
+    details: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+  },
+  {
+    id: 2,
+    techName: 'spaceport',
+    imgLandscape: '/src/images/technology/image-spaceport-landscape.jpg',
+    imgPortrait: '/src/images/technology/image-spaceport-portrait.jpg',
+    details: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+  },
+  {
+    id: 3,
+    techName: 'space capsule',
+    imgLandscape: '/src/images/technology/image-space-capsule-landscape.jpg',
+    imgPortrait: '/src/images/technology/image-space-capsule-portrait.jpg',
+    details: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+  },
 ]
 
-export { destinationsDB, crewsDB };
+export { destinationsDB, crewsDB, technologyDB };
