3cce66de38380e88ab6256def9f1885196802b7b
MYasin <yasin.ego10@gmail.com>
2023-05-26 22:33:31 +0800

    added functionality on technology page and make it responsive desktop to mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Navigation from './components/Navigation';
 // import LandingPage from './components/LandingPage';
 // import Destination from './components/Destination';
-import Crew from './components/Crew';
-// import TechnologyPage from './components/TechnologyPage';
+// import Crew from './components/Crew';
+import TechnologyPage from './components/TechnologyPage';
 function App() {
   return (
     <>
       <Navigation />
       {/* <LandingPage /> */}
       {/* <Destination /> */}
-      <Crew />
-      {/* <TechnologyPage /> */}
+      {/* <Crew /> */}
+      <TechnologyPage />
     </>
   )
 }
--- src/components/TechnologyPage.jsx
+++ src/components/TechnologyPage.jsx
@@ -1,43 +1,85 @@
+import { useState, useEffect } from "react";
+import { technologyDB } from "../utils/database";
 
 function TechnologyPage () {
+  const [selectedTech, setSelectedTech] = useState({
+    techName: '',
+    details: '',
+    imgLandscape: '',
+    imgPortrait: '',
+  })
+  const [isSelected, setIsSelected] = useState(1);
+
+  useEffect(() => {
+    setSelectedTech(technologyDB[0]);
+  },[])
+
+  function handleSelectedShip(e) {
+    const techSelected = e.target.textContent.toLowerCase();
+    console.log(techSelected);
+    const newTechSelected = technologyDB.find(tech => tech.id === Number(techSelected));
+    setSelectedTech(newTechSelected);
+  }
+
+  function activeTechSelected(num) {
+    setIsSelected(num);
+  }
+
   return (
     <div className="technology-bg">
-      <div className="technology">
 
-        <div className="technology-info-container">
-          <h5 className="sub-heading-5">
-            <span className="section-title">03</span>
-              Space Launch 101
-          </h5>
+      <div className="technology-info-container">
+        <h5 className="sub-heading-5">
+          <span className="section-title">03</span>
+            Space Launch 101
+        </h5>
 
-          <div className="technology-info">
-            <div className="btn-numbers">
-              <span className="btn-numbered-dot">1</span>
-              <span className="btn-numbered-dot">2</span>
-              <span className="btn-numbered-dot">3</span>
-            </div>
+        <div className="technology-info">
+          <div className="btn-numbers">
+            <span 
+              onClick={(e) => {
+                handleSelectedShip(e);
+                activeTechSelected(1)
+              }}
+              className={`btn-numbered-dot${isSelected === 1 ? ' space-active': ''}`}>1
+            </span>
+            
+            <span 
+              onClick={(e) => {
+                handleSelectedShip(e);
+                activeTechSelected(2)
+              }}
+              className={`btn-numbered-dot${isSelected === 2 ? ' space-active': ''}`}>2
+            </span>
 
-            <div className="technology-details">
-              <p className="body-text">THE TERMINOLOGY…</p>
-              <h3 className="heading-3">LAUNCH VEHICLE</h3>
-              <p className="body-text">
-                A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth&apos;s surface to space,
-                usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall,
-                it&apos;s quite an awe-inspiring sight on the launch pad!
-              </p>
-            </div>
-            <picture>
-              <source 
-                srcSet="/src/images/technology/image-launch-vehicle-landscape.png"
-                media="(max-width: 925px)"
-              />
-              <img src="/src/images/technology/image-launch-vehicle-portrait.jpg" alt="spaceship"/>
-            </picture>
+            <span 
+              onClick={(e) => {
+                handleSelectedShip(e);
+                activeTechSelected(3)
+              }}
+              className={`btn-numbered-dot${isSelected === 3 ? ' space-active': ''}`}>3
+            </span>
           </div>
 
+          <div className="technology-details">
+            <p className="body-text">THE TERMINOLOGY…</p>
+            <h3 className="heading-3">{selectedTech.techName}</h3>
+            <p className="body-text">
+              {selectedTech.details}
+            </p>
+          </div>
         </div>
 
+        <picture>
+          <source 
+            srcSet={selectedTech.imgLandscape}
+            media="(max-width: 960px)"
+          />
+          <img src={selectedTech.imgPortrait} alt={selectedTech.techName}/>
+        </picture>
+
       </div>
+
     </div>
   )
 }
