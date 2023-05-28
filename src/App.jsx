171691b831ddcb44a20372c12ef863bcf0ee24fd
MYasin <yasin.ego10@gmail.com>
2023-05-28 21:23:50 +0800

    refactor the landing page to be more responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Navigation from './components/Navigation';
-// import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage';
 // import Destination from './components/Destination';
 // import Crew from './components/Crew';
-import TechnologyPage from './components/TechnologyPage';
+// import TechnologyPage from './components/TechnologyPage';
 function App() {
   return (
     <>
       <Navigation />
-      {/* <LandingPage /> */}
+      <LandingPage />
       {/* <Destination /> */}
       {/* <Crew /> */}
-      <TechnologyPage />
+      {/* <TechnologyPage /> */}
     </>
   )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,14 +2,14 @@
 function LandingPage() {
   return (
     <div className='landing-bg'>
-      <div className='landing-page'>
+      <div className='landing-page'> 
           <div className='left-section'>
             <h5 className='sub-heading-5'>SO, YOU WANT TO TRAVEL TO</h5>
             <h1 className='heading'>SPACE</h1>
             <p className='body-text'>Let&apos;s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we&apos;ll give you a truly out of this world experience!</p>
           </div>
             <div className='btn-hover'>
-              <button className='btn-explore'>EXPLORE</button>
+              <a className='btn-explore'>EXPLORE</a>
           </div>
       </div>
     </div>
